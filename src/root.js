41ea41f7ac91e5a12b01ab7a56637d8f8752b4e5
SCJMENGMENG <j2289420119@163.com>
2018-06-06 16:33:54 +0800

    amend
    警告去除

--- src/root.js
+++ src/root.js
@@ -3,6 +3,10 @@ import {AppRegistry} from 'react-native'
 
 import {AppNavigator} from './navigators/navigators';
 
+//屏蔽警告
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 class Root extends React.Component{
 
     constructor(props){
